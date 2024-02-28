ee7380534fc3b5f0c76bca62b50a60d1fe03daa5
MchaC1 <chad.mchardy@sasktel.com>
2024-02-28 13:22:18 -0600

    another env var try

--- app.js
+++ app.js
@@ -12,8 +12,9 @@ dotenv.config()
 console.log('CAM interesting');
 console.log(`CAM PORT: ${process.env.PORT}`);
 console.log(`CAM DB_USER: ${process.env.DB_USER}`);
-console.log(`CAM process.env:`);
-console.log(process.env);
+// console.log(`CAM DB_USER 2: ${DB_USER}`);
+// console.log(`CAM process.env:`);
+// console.log(process.env);
 
 try {
     db.connectAnother();
